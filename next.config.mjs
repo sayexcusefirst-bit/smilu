2617c2683ac8b8e267d66f4fbf943e0b494a646c
sayexcusefirst-bit <sayexcusefirst@gmail.com>
2026-04-21 14:14:16 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./prisma/vocab.db'],
+    },
+  },
 };
 
 export default nextConfig;
